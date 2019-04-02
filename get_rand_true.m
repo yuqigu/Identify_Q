00e38e5474dabe7011e7c2cf26e0b5fc5fb62fd3
Yuqi Gu <yuqigu@umich.edu>
2019-04-02 02:47:30 -0400

    modify true parameter generating mechanism

--- get_rand_true.m
+++ get_rand_true.m
@@ -9,11 +9,10 @@ g_true = zeros(J, Nset);
 
 rng('default');
 for rr = 1:Nset
-    % pn = 0.1 * rand(4,1) + 0.1;
-    pn = gamrnd(ones(1,2^K), ones(1,2^K));
+    pn = gamrnd(3*ones(1,2^K), ones(1,2^K));
     p_true(:,rr) = pn / sum(pn);
-    c_true(:,rr) = 0.9 - 0.3 * rand(J, 1);
-    g_true(:,rr) = 0.1 + 0.3 * rand(J, 1);
+    c_true(:,rr) = 0.9 - 0.2 * rand(J, 1);
+    g_true(:,rr) = 0.1 + 0.2 * rand(J, 1);
 end
     
 
